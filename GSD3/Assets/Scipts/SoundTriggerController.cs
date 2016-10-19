71f9d904a021b011ffb0c2810debac5cafcb5b2a
Milton van de Sanden <milton.vandesanden@gmail.com>
2016-10-19 10:53:07 +0200

    implemented door opening sound

---
+++ GSD3/Assets/Scipts/SoundTriggerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTriggerController : MonoBehaviour
+{
+    public AudioSource audioSourceEnter;
+    public AudioSource audioSourceExit;
+
+	// Use this for initialization
+	void Start(){}
+	
+	// Update is called once per frame
+	void Update(){}
+
+    void OnTriggerEnter(Collider col)
+    {
+        audioSourceEnter.Play();
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+
+    }
+
+}
